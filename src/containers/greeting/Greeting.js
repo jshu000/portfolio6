565a4cfa6810855c8e64ba794fe1b82e6639b073
Jshu <56064412+jshu000@users.noreply.github.com>
2024-10-07 13:01:39 +0530

    Update
    Update

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -39,15 +39,6 @@ export default function Greeting() {
               <SocialMedia />
               <div className="button-greeting-div">
                 <Button text="Contact me" href="#contact" />
-                {greeting.resumeLink && (
-                  <a
-                    href={require("./resume.pdf")}
-                    download="Resume.pdf"
-                    className="download-link-button"
-                  >
-                    <Button text="Download my resume" />
-                  </a>
-                )}
               </div>
             </div>
           </div>
